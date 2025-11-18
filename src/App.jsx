56cd85b4108b961b68a81be3e5e71f319deab987
AbdullahYounas <abdullahyounas24082005@gmail.com>
2025-11-18 22:34:48 +0500

    V4.91

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { gsap } from "gsap";
+import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Hero from './components/Hero';
 import Features from './components/Features';
